c96e1b287bf08b7732c7409e174850f977fec1d2
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-01-08 10:23:43 +0700

    js su dung toan tu

---
+++ Module1/Week2/js-su-dung-toan-tu/index.js
@@ -0,0 +1,11 @@
+let inputWidth = prompt('Nhập vào chiều dài: ');
+let inputHeight = prompt('Nhập vào chiều rộng: ');
+
+inputWidth = parseInt(inputWidth);
+inputHeight = parseInt(inputHeight);
+
+let area = inputWidth * inputHeight
+
+// alert('Diện tích hình chữ nhật = ' + area);
+
+document.write("The area is: " + area);
