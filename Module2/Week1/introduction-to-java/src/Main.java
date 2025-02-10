b861a346ebbc919c0e0b631b1e71c28634a3db5f
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-02-10 15:28:08 +0700

    add variable datatype

---
+++ Module2/Week1/introduction-to-java/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Nhap vao 1 so: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Number: " + number);
+    }
+}
