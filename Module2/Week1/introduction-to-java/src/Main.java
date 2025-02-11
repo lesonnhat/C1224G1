c2ff729102e18af342257ead0305123917753bdb
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-02-11 10:06:42 +0700

    add week 1 module 2

--- Module2/Week1/introduction-to-java/src/Main.java
+++ Module2/Week1/introduction-to-java/src/Main.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Nhap vao 1 so: ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println("Number: " + number);
+//        System.out.println("Nhap vao 1 so: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        System.out.println("Number: " + number);
+
+        Scanner sc = new Scanner(System.in);
+        String password;
+        do {
+            System.out.print("Enter your password: ");
+            password = sc.nextLine();
+        } while (!password.equals("vung oi mo ra"));
     }
 }
--- Module2/Week1/th-giai-pt-bac-nhat/src/LuyenTap.java
+++ Module2/Week1/th-giai-pt-bac-nhat/src/LuyenTap.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
-public class LinearEquationResolver {
+public class LuyenTap {
     public static void main(String[] args) {
-        System.out.println("Linear Equation Resolver");
-        System.out.println("Given a equation as 'a * x + b = c', please enter constants:");
+        System.out.println("Giải Phương Trình Bậc Nhất");
+        System.out.println("Phương trình có dạng 'a * x + b = c', vui lòng nhập hệ số");
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("a: ");
         double a = scanner.nextDouble();
 
@@ -15,13 +17,13 @@ public class LinearEquationResolver {
         double c = scanner.nextDouble();
 
         if (a != 0) {
-            double answer = (c - b) / a;
-            System.out.printf("Equation pass with x = %f!\n", answer);
+            double x = (c - b) / a;
+            System.out.print("x = " + x);
         } else {
             if (b == c) {
-                System.out.print("The solution is all x!");
+                System.out.print("Phương trình vô số nghiệm");
             } else {
-                System.out.print("No solution!");
+                System.out.print("Phương trình vô nghiệm");
             }
         }
     }
