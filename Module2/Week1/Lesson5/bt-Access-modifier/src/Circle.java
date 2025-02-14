9f90c88f030d478abcb5fee7dc25c74aa4e77fc8
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-02-14 11:13:10 +0700

    add bt Access modifier

---
+++ Module2/Week1/Lesson5/bt-Access-modifier/src/Circle.java
@@ -0,0 +1,20 @@
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle(){
+    }
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
